f9260abf2a2f193dcdf5f433f258538c3ac72be8
Gabriel Paiva <gabriel.paiva@dasa.com.br>
2021-04-22 17:13:55 -0300

    chore: removed env files from configs

--- services/firebase.ts
+++ services/firebase.ts
@@ -3,13 +3,13 @@ import 'firebase/analytics'
 import 'firebase/firestore'
 
 const config = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 }
 
 export const getDatabase = () => {
@@ -18,7 +18,6 @@ export const getDatabase = () => {
   if (!firebase.apps.length) {
     const app = firebase.initializeApp(config)
     db = firebase.firestore(app)
-    // firebase.analytics()
   } else {
     const app = firebase.app()
     db = firebase.firestore(app)
